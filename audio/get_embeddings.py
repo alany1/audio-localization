666f2417be803263297b2be8d55b22a52d7f4fba
alany1 <alanyu@csail.mit.edu>
2023-04-20 13:21:57 -0400

    refactor

--- audio/get_embeddings.py
+++ audio/get_embeddings.py
@@ -6,6 +6,7 @@ import numpy as np
 class AudioArgs(ParamsProto):
     source_path: str = Proto(help='Path to the audio file.')
     # model_str = Proto(type=str, default='', help='Model to use for embeddings.')
+
 def get_embeddings(audio_path):
     """Returns the embeddings of the audio file at the given path."""
     print("Getting embeddings for audio file at path: {}".format(AudioArgs.source_path))
