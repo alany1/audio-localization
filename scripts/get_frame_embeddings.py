15e5ba535cda4f8e53301755d1a37e8b0ca0f177
alany1 <alanyu@csail.mit.edu>
2023-04-20 20:35:48 -0400

    Make viz

--- scripts/get_frame_embeddings.py
+++ scripts/get_frame_embeddings.py
@@ -28,14 +28,14 @@ def get_frame_embedding(images):
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     print("Loading CLIP model on device: {}".format(device))
-    model, preprocess = clip.load('ViT-B/16', device=device)
+    model, preprocess = clip.load('ViT-L/14@336px', device=device)
     print("Loaded CLIP model on device: {}".format(device))
 
     # Preprocess each image
     # start_time = time.perf_counter()
     # with logger.time("clip_preprocess_images"):
     preprocessed_images = torch.stack([preprocess(image) for image in images])
-    preprocessed_images = preprocessed_images.to(device)  # (b, 3, 336, 336)
+    preprocessed_images = preprocessed_images.to(device)  # (b, 3, 336, 336) depending on the model preprocessing
 
     patch_embeddings = []
     for i in range(0, len(images), VideoArgs.batch_size):
@@ -43,36 +43,36 @@ def get_frame_embedding(images):
         # Change to half
         batch = batch.half()
         # patch_embeddings.append(model.visual(batch, patch_output=True))
-        patch_embeddings.append(model.get_patch_encodings(batch))
+        patch_embeddings.append(model.visual(batch, patch_output = True))
     patch_embeddings = torch.cat(patch_embeddings, dim=0)
 
     # Reshape patch_embeddings into (N, 24, 24, embedding_dim)
     patch_embeddings = patch_embeddings.reshape(-1, 24, 24, patch_embeddings.shape[-1])
 
     return patch_embeddings
-    image = preprocess(frame).unsqueeze(0)
-    with torch.no_grad():
-        features = model.visual(image, patch_output = True)  # Get the image features
-        # Reshape the features into patches
-        patches = features.reshape(1, features.shape[1], features.shape[2], -1)
-        patch_embeddings = patches.squeeze(0).permute(2, 0, 1)
-    return patch_embeddings
-    # Define the image transform and load the image
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-    ])
-    print("Transforming frame")
-    img = transform(frame).unsqueeze(0).to(device)
-    # convert to half tensor
-    img = img.half()
 
-    # Extract the image patches
-    image_patches = model.visual(img)
+def visualize_embeddings(embedding):
+    """
+    Visualize the first three principal components of embedding to correspond to RGB.
+    """
+    import matplotlib.pyplot as plt
+    import torch
+    from sklearn.decomposition import PCA
 
-    # Compute the embeddings for each patch
-    image_patch_embeddings = model.visual(image_patches).squeeze(2).squeeze(2)
-    return image_patch_embeddings
+    # Reshape the embedding into (N, embedding_dim)
+    embedding = embedding.reshape(-1, embedding.shape[-1])
+
+    # Apply PCA to reduce the dimensionality of the embedding
+    pca = PCA(n_components=3)
+    pca.fit(embedding.detach().cpu().numpy())
+    embedding = torch.from_numpy(pca.transform(embedding.detach().cpu().numpy()))
+
+    # Reshape the embedding back into (N, 24, 24, 3)
+    embedding = embedding.reshape(-1, 24, 24, 3)
+    return embedding
+
+
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     # VideoArgs.source_path = 'examples/ocean-wave-1.wav'
@@ -83,12 +83,13 @@ if __name__ == '__main__':
     video, audio, info = io.read_video(VideoArgs.source_path, pts_unit='sec')
     # video_tensor = video.permute(0, 3, 1, 2)
     video_tensor = video
-    print(video_tensor.shape)
-
+    print("Frames count = {}".format(video_tensor.shape[0]))
     # Get embeddings for each frame
 
     model = wav2clip.get_model(frame_length=16000, hop_length=16000)
 
     frame_pil = Image.fromarray(video_tensor[0].numpy(), mode='RGB')
     embeddings = get_frame_embedding([frame_pil])
-    # embeddings = wav2clip.embed_audio(np.array(video_tensor[0]), model)
+
+    viz = visualize_embeddings(embeddings).detach().cpu().numpy()
+
